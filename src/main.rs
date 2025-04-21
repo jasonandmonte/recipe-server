0c9e635e86e6b9104523b7ebb181a06dd92109f9
jasonandmonte <jason.monte55@gmail.com>
2025-04-21 16:11:19 -0700

    Create server
    Setup starting server that uses recipe struct with a constant.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
-fn main() {
-    println!("Hello, world!");
+mod recipe;
+mod templates;
+
+use recipe::*;
+use templates::*;
+
+extern crate mime;
+use axum::{self, response, routing};
+use tokio::net;
+use tower_http::services;
+
+async fn get_recipe() -> response::Html<String> {
+    // Reference to a constant
+    let joke = IndexTemplate::recipe(&THE_RECIPE);
+    response::Html(joke.to_string())
+}
+
+async fn serve() -> Result<(), Box<dyn std::error::Error>> {
+    let mime_favicon = "image/vnd.microsoft.icon".parse().unwrap();
+    let app = axum::Router::new()
+        .route("/", routing::get(get_recipe))
+        // NOTE: axum talks to tower-http
+        .route_service(
+            "/recipe.css",
+            services::ServeFile::new_with_mime(
+                "assets/static/recipe.css",
+                &mime::TEXT_CSS_UTF_8,
+            ),
+        )
+        .route_service(
+            "/favicon.ico",
+            services::ServeFile::new_with_mime(
+                "assets/static/favicon.ico",
+                &mime_favicon,
+            ),
+        );
+    let listener = net::TcpListener::bind("127.0.0.1:3000").await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = serve().await {
+        eprintln!("recipes: error: {}", err);
+        std::process::exit(1);
+    }
 }
