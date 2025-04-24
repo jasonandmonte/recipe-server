f464d044e7e507f70d79912e8c2361675b40457d
jasonandmonte <jason.monte55@gmail.com>
2025-04-24 14:29:11 -0700

    Implement RecipeError
    Implement RecipeError for read_recipes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
+mod error;
 mod recipe;
 mod templates;
 
+use error::*;
 use recipe::*;
 use templates::*;
 
 extern crate mime;
 use axum::{self, extract::State, response, routing};
+use std::sync::Arc;
 use tokio::{net, sync::RwLock};
 use tower_http::services;
-use std::sync::Arc;
 
 struct AppState {
     recipes: Vec<Recipe>,
@@ -25,8 +27,7 @@ async fn get_recipe(State(app_state): State<Arc<RwLock<AppState>>>) -> response:
 
 async fn serve() -> Result<(), Box<dyn std::error::Error>> {
     let recipes = read_recipes("assets/static/recipes.json")?;
-    let state = Arc::new(RwLock::new(AppState{recipes}));
-
+    let state = Arc::new(RwLock::new(AppState { recipes }));
 
     let mime_favicon = "image/vnd.microsoft.icon".parse().unwrap();
     let app = axum::Router::new()
@@ -34,17 +35,11 @@ async fn serve() -> Result<(), Box<dyn std::error::Error>> {
         // NOTE: axum talks to tower-http
         .route_service(
             "/recipe.css",
-            services::ServeFile::new_with_mime(
-                "assets/static/recipe.css",
-                &mime::TEXT_CSS_UTF_8,
-            ),
+            services::ServeFile::new_with_mime("assets/static/recipe.css", &mime::TEXT_CSS_UTF_8),
         )
         .route_service(
             "/favicon.ico",
-            services::ServeFile::new_with_mime(
-                "assets/static/favicon.ico",
-                &mime_favicon,
-            ),
+            services::ServeFile::new_with_mime("assets/static/favicon.ico", &mime_favicon),
         )
         .with_state(state);
     let listener = net::TcpListener::bind("127.0.0.1:3000").await?;
--- src/recipe.rs
+++ src/recipe.rs
@@ -1,15 +1,16 @@
-use std::path::Path;
+use crate::RecipeError;
 use serde::Deserialize;
+use std::path::Path;
 
 #[derive(Deserialize)]
 pub struct Recipe {
     pub title: String,
-    pub ingredients : Vec<String>,
+    pub ingredients: Vec<String>,
     pub instructions: Vec<String>,
     pub source: String,
 }
 
-pub fn read_recipes<P: AsRef<Path>>(recipes_path: P) -> Result<Vec<Recipe>, Box<dyn std::error::Error>> {
+pub fn read_recipes<P: AsRef<Path>>(recipes_path: P) -> Result<Vec<Recipe>, RecipeError> {
     let f = std::fs::File::open(recipes_path.as_ref())?;
     let recipes = serde_json::from_reader(f)?;
     Ok(recipes)
