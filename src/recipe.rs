1d44c6636d4864b364ad5147062c6264c202b50f
jasonandmonte <jason.monte55@gmail.com>
2025-05-05 17:50:42 -0700

    Update Recipe structure
    Segment between Recipe and JSONRecipe.

--- src/recipe.rs
+++ src/recipe.rs
@@ -1,17 +1,43 @@
 use crate::RecipeError;
 use serde::Deserialize;
-use std::path::Path;
+use std::{collections::HashSet, ops::Deref, path::Path};
 
 #[derive(Deserialize)]
-pub struct Recipe {
+pub struct JSONRecipe {
+    pub id: String,
     pub title: String,
-    pub ingredients: Vec<String>,
-    pub instructions: Vec<String>,
+    pub ingredients: String,
+    pub instructions: String,
     pub source: String,
+    pub tags: HashSet<String>,
 }
 
-pub fn read_recipes<P: AsRef<Path>>(recipes_path: P) -> Result<Vec<Recipe>, RecipeError> {
+
+pub struct Recipe {
+    pub id: String,
+    pub title: String,
+    pub ingredients: String,
+    pub instructions: String,
+    pub recipe_source: String,
+}
+
+pub fn read_recipes<P: AsRef<Path>>(recipes_path: P) -> Result<Vec<JSONRecipe>, RecipeError> {
     let f = std::fs::File::open(recipes_path.as_ref())?;
     let recipes = serde_json::from_reader(f)?;
     Ok(recipes)
+}
+
+impl JSONRecipe {
+    pub fn to_recipe(&self) -> (Recipe, impl Iterator<Item=&str>) {
+        let recipe = Recipe {
+            id: self.id.clone(),
+            title: self.title.clone(),
+            ingredients: self.ingredients.clone(),
+            instructions: self.instructions.clone(),
+            recipe_source: self.source.clone(),
+        };
+
+        let tags = self.tags.iter().map(String::deref);
+        (recipe, tags)
+    }
 }
